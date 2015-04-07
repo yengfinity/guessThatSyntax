c38a93ed05e997214fd5da03abdb58931d5ae3a6
Yeng <leeyeng2005@gmail.com>
2015-04-07 16:07:04 -0500

    Working pop-up modals

--- js/script.js
+++ js/script.js
@@ -19,21 +19,36 @@ Card.prototype.printMe = function() {
 	'<img src=\" ' + this.pic + '\">' +
 	"<br/><h3>" + this.cardName + "</h3>" +
 	"<br/> Family is: " + this.family +
-	"<br/> Description: " + this.desc ;
+	"<br/> Description: " + this.desc;
+
+};
+
+//Print the short version of printMe before click
+Card.prototype.printMeShort = function() {
+	//Print Cards to HTML
+	var element = document.getElementById(this.cardId);
+	return element.innerHTML = 
+	'<img src=\" ' + this.pic + '\">' +
+	"<br/><h3>" + this.cardName + "</h3>" +
+	"<button> Button Here </button>";
 
 };
 
 //Declare new card objects
 var card1 = new Card('HTML', 'html', 'Stands for Hyper Markup Language', 'img/profile.png', 'card1');
-var card2 = new Card('Head', 'html', 'Needed to styles and scripts', 'img/profile.png', 'card2');;
-var card3 = new Card('Body', 'html', 'Needed to styles and scripts', 'img/profile.png', 'card3');;
+var card2 = new Card('Head', 'html', 'Needed include external style sheets and scripts', 'img/profile.png', 'card2');;
+var card3 = new Card('Body', 'html', 'This is needed to make markup show up', 'img/profile.png', 'card3');;
 var card4 = new Card('Footer', 'html', 'Bottom tag to include info', 'img/profile.png', 'card4');;
 
+//Print selected card to div
+card1.printMe();
+card2.printMe();
+card3.printMe();
+card4.printMe();
 
 
 //display random card onload
 window.onload = function() {
-  card.printMe();
   doSomethingElse();
 };
 
@@ -41,13 +56,9 @@ function addHandler(card){
     document.getElementById(this).onclick = card.printMe();
     };
 
-//Print selected card to div
-card1.printMe();
-card2.printMe();
-card3.printMe();
 
 //loop to close which ever boxes were added in array
-var boxArray = ['box1','box2','box3'];
+var boxArray = ['box1','box2','box3', 'box4'];
 window.addEventListener('mouseup', function(event){
 	for(var i=0; i < boxArray.length; i++){
 	    var box = document.getElementById(boxArray[i]);
